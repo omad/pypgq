4fda4653a7ced0c8beca2dd9fa75ad5914c3048b
Damien Ayers <damien@omad.net>
2019-11-19 12:06:00 +1100

    Testing code

--- pg_que/cli.py
+++ pg_que/cli.py
@@ -38,18 +38,18 @@ async def run_perf_tests(n=10000):
     end = time.perf_counter()
     took = end - start
 
-    print(f'Took {took} to send {n} messages. {took / n} per message.')
+    print(f'Took {took} to send {n + 1} messages. {took / (n + 1)} per message.')
 
     start = time.perf_counter()
     more_messages = True
     while more_messages:
-        m = await queue.receive_messages(batch_size=1)
+        m = await queue.receive_messages(batch_size=10)
         if not m:
             more_messages = False
     end = time.perf_counter()
     took = end - start
 
-    print(f'Took {took} to receive {n} messages. {took / n} per message.')
+    print(f'Took {took} to receive {n + 1} messages. {took / (n + 1)} per message.')
 
 
 if __name__ == '__main__':
